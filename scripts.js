e1141d2abf484cbe0a098109c7ac7a23fc20a2da
Luana Vallejos <luanalorenavallejos@gmail.com>
2021-11-08 15:07:45 -0300

    subo archivos a github

---
+++ scripts.js
@@ -0,0 +1,14 @@
+/*let productos [];
+let total=0;
+
+
+function agregar (producto,precio) {
+    console.log(producto,precio);
+    productos.push(producto);
+    total = total + precio;
+}
+
+function pagar () {
+    console.log(producto,precio);
+}
+
